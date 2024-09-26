a4fb601bbead0cb3776ae99c259633586d80768e
Kevin Sanchez <Chilledsonic.ks2@gmail.com>
2024-09-26 13:00:32 -0400

    Finished Dashboard

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 
+import { CheckCheck, Package, Tag, TrendingDown, TrendingUp } from "lucide-react";
 import CardExpenseSummary from "./CardExpenseSummary";
 import CardPopularProducts from "./CardPopularProducts";
 import CardPurchaseSummary from "./CardPurchaseSummary";
 import CardSalesSummary from "./CardSalesSummary";
+import StatCard from "./StatCard";
 
 const Dashboard = () => {
   return <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
@@ -11,9 +13,33 @@ const Dashboard = () => {
     <CardSalesSummary />
     <CardPurchaseSummary />
     <CardExpenseSummary />
-    <div className="md:row-span-1 xl:row-span-2 bg-gray-500" />
-    <div className="md:row-span-1 xl:row-span-2 bg-gray-500" />
-    <div className="md:row-span-1 xl:row-span-2 bg-gray-500" />
+    <StatCard 
+    title="Customer & Expenses"
+    primaryIcon={<Package className="text-blue-600 w-6 h-6" />}
+    dateRange="22 - 29 October 2023"
+    details= {[
+      { title: "Customer Growth", amount: "175.00", changePercentage: 131, IconComponent: TrendingUp},
+      { title: "Expenses", amount: "10", changePercentage: -56, IconComponent: TrendingDown}
+    ]}
+    />
+   <StatCard 
+    title="Dues & Pending Orders"
+    primaryIcon={<CheckCheck className="text-blue-600 w-6 h-6" />}
+    dateRange="22 - 29 October 2023"
+    details= {[
+      { title: "Dues", amount: "250.00", changePercentage: 131, IconComponent: TrendingUp},
+      { title: "Pending Orders", amount: "145", changePercentage: -56, IconComponent: TrendingDown}
+    ]}
+    />
+    <StatCard 
+    title="Sales & Discount"
+    primaryIcon={<Tag className="text-blue-600 w-6 h-6" />}
+    dateRange="22 - 29 October 2023"
+    details= {[
+      { title: "Sales", amount: "1000.00", changePercentage: 131, IconComponent: TrendingUp},
+      { title: "Discount", amount: "200", changePercentage: -10, IconComponent: TrendingDown}
+    ]}
+    />
   </div>;
 };
 
